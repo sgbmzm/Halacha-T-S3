a9285209c54a62069c435c51646c6cb523225da3
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2025-02-16 08:25:33 +0200

    Update main.py

--- drivers/main.py
+++ drivers/main.py
@@ -6,7 +6,7 @@ button_14 = Pin(14, Pin.IN, Pin.PULL_UP) # משמש בקוד להכנסת המכ
 
 # אם שני הכפתורים לחוצים בעשה שהמכשיר מאותחל אז קוראים לפוקציית עדכון שעון ההלכה
 if boot_button.value() == 0 and button_14.value() == 0:
-    import update_halacha_clock
+    from halacha_clock import update_halacha_clock
 # בכל מקרה אחר מפעילים את שעון ההלכה
 else:
-    from halacha_clock import main_shemesh_s3 #########
+    from halacha_clock import main_shemesh_s3 
