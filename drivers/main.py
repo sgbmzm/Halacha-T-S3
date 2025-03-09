5f6c7862c24ed4a38d0c4ecdc2b0cad28c37ca50
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2025-03-09 14:20:34 +0200

    Update main.py
    הפסקת טיפול ב BME280 כאן, כי מטופל בקוד הראשי של main_shemesh_s3

--- drivers/main.py
+++ drivers/main.py
@@ -9,14 +9,4 @@ if boot_button.value() == 0 and button_14.value() == 0:
     from halacha_clock import update_halacha_clock
 # בכל מקרה אחר מפעילים את שעון ההלכה
 else:
-        
-    try:
-        # אם מחובר חיישן טמפרטורה ולחות מפעילים את תוכנת מזג האוויר
-        from machine import Pin, I2C
-        from halacha_clock import bme280
-        original_i2c = I2C(scl=Pin(44), sda=Pin(43))
-        bme = bme280.BME280(i2c=original_i2c)
-        from halacha_clock import main_bme280_s3
-    except:
-        # אם הוא לא מחובר מקבלים שגיאה ואז מפעילים את שעון ההלכה
-        from halacha_clock import main_shemesh_s3
+    from halacha_clock import main_shemesh_s3
