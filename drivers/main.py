35302436974bfce583a864058a8f009a75a8eb40
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2025-03-17 22:14:23 +0200

    Update main.py
    הוספת הודעה "מתחיל" לפני תחילת הקוד כי לוקח לקוד זמן להיטען ובינתיים לא כדאי שהמסך יהיה ריק ויחשבו שיש בעיה

--- drivers/main.py
+++ drivers/main.py
@@ -1,4 +1,26 @@
-from machine import Pin
+from machine import Pin, PWM
+
+# למסך
+import halacha_clock.tft_config as tft_config
+import s3lcd
+import halacha_clock.miriam25 as FontHeb25
+
+# פונקצייה להפיכת טקסט כדי שעברית תהיה משמאל לימין
+def reverse(string):
+    return "".join(reversed(string))
+
+# הצגת הודעה על המסך כמה שיותר מהר כדי שיראו שהכל בסדר
+try:
+    tft = tft_config.config(rotation=3) # כיוון סיבוב התצוגה שאני רוצה
+    tft.init() # כך חייבים לעשות
+    BACKLIGHT = PWM(Pin(38, Pin.OUT), freq=1000)
+    BACKLIGHT.duty(255)
+    tft.fill(0) # מחיקת המסך
+    tft.write(FontHeb25,f'{reverse("מתחיל...")}',200,20)
+    #tft.line(0, 45, 320, 45, s3lcd.YELLOW)
+    tft.show()
+finally:
+    tft_config.deinit(tft)
 
 # הגדרת הכפתורים הפיזיים במכשיר
 boot_button = Pin(0, Pin.IN, Pin.PULL_UP) # משמש בקוד לשינוי המיקומים ולקביעת מיקום ברירת מחדל
@@ -7,6 +29,8 @@ button_14 = Pin(14, Pin.IN, Pin.PULL_UP) # משמש בקוד להכנסת המכ
 # אם שני הכפתורים לחוצים בעשה שהמכשיר מאותחל אז קוראים לפוקציית עדכון שעון ההלכה
 if boot_button.value() == 0 and button_14.value() == 0:
     from halacha_clock import update_halacha_clock
+
 # בכל מקרה אחר מפעילים את שעון ההלכה
 else:
     from halacha_clock import main_shemesh_s3
+
