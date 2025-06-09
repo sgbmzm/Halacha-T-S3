86ac8aa236fa7678f9e454d31b9d470f61b3792c
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2025-06-09 19:15:11 +0300

    Add files via upload

---
+++ rom/רשימת הפינים של S3.py
@@ -0,0 +1,35 @@
+# הגדרת פינים עבור ESP32S3
+
+PIN_LCD_BL = 38
+
+PIN_LCD_D0 = 39
+PIN_LCD_D1 = 40
+PIN_LCD_D2 = 41
+PIN_LCD_D3 = 42
+PIN_LCD_D4 = 45
+PIN_LCD_D5 = 46
+PIN_LCD_D6 = 47
+PIN_LCD_D7 = 48
+
+PIN_POWER_ON = 15
+
+PIN_LCD_RES = 5
+PIN_LCD_CS = 6
+PIN_LCD_DC = 7
+PIN_LCD_WR = 8
+PIN_LCD_RD = 9
+
+PIN_BUTTON_1 = 0
+PIN_BUTTON_2 = 14
+PIN_BAT_VOLT = 4  # פין למדידת מתח סוללה
+
+PIN_IIC_SCL = 17  # SCL של I2C
+PIN_IIC_SDA = 18  # SDA של I2C
+
+PIN_TOUCH_INT = 16
+PIN_TOUCH_RES = 21
+
+# הרחבות חיצוניות (External Expansion)
+PIN_SD_CMD = 13
+PIN_SD_CLK = 11
+PIN_SD_D0 = 12
